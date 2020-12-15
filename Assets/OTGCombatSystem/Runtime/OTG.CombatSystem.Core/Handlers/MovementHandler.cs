317383fc52368ee8b3f26212da45701fed0b05a3
Jason <jrespress@otgstudio.online>
2020-12-15 17:25:25 -0600

    Movehandler Cleanup

--- Assets/OTGCombatSystem/Runtime/OTG.CombatSystem.Core/Handlers/MovementHandler.cs
+++ Assets/OTGCombatSystem/Runtime/OTG.CombatSystem.Core/Handlers/MovementHandler.cs
@@ -5,15 +5,33 @@ namespace OTG.CombatSM.Core
 {
     public class MovementHandler
     {
-        
+
         #region Properties
-        public TwitchMovementParams TwitchParams { get; private set; }
+        public Transform Comp_Transform { get; private set; }
+        public MovementHandlerData Data { get; private set; }
+        public CharacterController Comp_CharacterControl { get; private set; }
+        public OTGGlobalCombatConfig GlobalCombatConfig { get; private set; }
+        #endregion
+
+        #region Parameters
+        public float Speed_DesiredXaxis;
+        public float Speed_DesiredYaxis;
+        public float Speed_DesiredZaxis;
+
+        public float Speed_CurrentXaxis;
+        public float Speed_CurrentYaxis;
+        public float Speed_CurrentZaxis;
+
+
         #endregion
 
         #region Public API
         public MovementHandler(HandlerDataGroup _dataGroup, CharacterController _charControl, Transform _trans, OTGGlobalCombatConfig _globalConfig)
         {
-            TwitchParams = new TwitchMovementParams(_trans,_charControl, _dataGroup.MoveHandlerData, _globalConfig);
+            Data = _dataGroup.MoveHandlerData;
+            Comp_CharacterControl = _charControl;
+            Comp_Transform = _trans;
+            GlobalCombatConfig = _globalConfig;
         }
         public void CleanupHandler()
         {
@@ -26,69 +44,12 @@ namespace OTG.CombatSM.Core
         #region Utility
         private void Cleanup()
         {
-            TwitchParams.Cleanup();
-            TwitchParams = null;
+            Data = null;
+            Comp_CharacterControl = null;
+            Comp_Transform = null;
+            GlobalCombatConfig = null;
         }
 
         #endregion
-    }
-    public class TwitchMovementParams
-    {
-        public const int FRONT_LANE = -1;
-        public const int BACK_LANE = 1;
-        public const int MIDDLE_LANE = 0;
-       
-        public Transform Comp_Transform { get; private set; }
-        public CharacterController Comp_CharacterControl { get; private set; }
-        public TwitchMovementData Data { get; private set; }
-        public OTGGlobalCombatConfig GlobalCombatConfig { get; private set; }
-
-        public Vector3 DashStartPosition;
-        public float DesiredDashDistance;
-        public float CurrentDashDistance;
-        public float DesiredDashSpeed;
-        public float PercentageDistanceTraveled;
-
-        public int CurrentLane;
-        public int TargetLane;
-
-        public float HorizontalSpeed;
-        public float VerticalSpeed;
-        public float DepthSpeed;
-        
-        public TwitchMovementParams(Transform _trans, CharacterController _control, MovementHandlerData _data, OTGGlobalCombatConfig _globalCOnfig)
-        {
-            Comp_Transform = _trans;
-            Comp_CharacterControl = _control;
-            Data = _data.TwitchData;
-            GlobalCombatConfig = _globalCOnfig;
-        }
-        public void Cleanup()
-        {
-            Comp_Transform = null;
-            Comp_CharacterControl = null;
-            Data = null;
-            GlobalCombatConfig = null;
-
-            ResetParams();
-        }
-        public void ResetParams()
-        {
-            DashStartPosition = Vector3.zero;
-            DesiredDashSpeed = 0;
-            DesiredDashDistance = 0;
-            CurrentDashDistance = 0;
-            HorizontalSpeed = 0;
-            VerticalSpeed = 0;
-            DepthSpeed = 0;
-        }
-        public void RecieveDamagePayload(IDamagePayload _payload)
-        {
-            Vector3 impactForce = _payload.GetImpactForce();
-            HorizontalSpeed = impactForce.x;
-            VerticalSpeed = impactForce.y;
-            DepthSpeed = impactForce.z;
-           
-        }
     }
 }
